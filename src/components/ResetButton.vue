5be516eb232f3fd80c4b384c366f5319ea91c6e8
Sanaz Bahmani <sanazbahmani35@gmail.com>
2024-05-28 16:45:43 +0330

    Implement the design for the Result section

---
+++ src/components/ResetButton.vue
@@ -0,0 +1,19 @@
+<script setup>
+import { NButton } from "naive-ui";
+</script>
+
+<template>
+  <NButton class="button" color="#26c0ab">RESET</NButton>
+</template>
+
+<style scoped>
+.button {
+  width: 100%;
+  color: hsl(183, 100%, 15%);
+}
+
+.button:hover {
+  background-color: hsl(185, 41%, 84%);
+  color: hsl(183, 100%, 15%);
+}
+</style>
--- src/components/Result.vue
+++ src/components/Result.vue
@@ -1,24 +1,57 @@
-<script setup></script>
+<script setup>
+import ResetButton from "./ResetButton.vue";
+</script>
 
 <template>
-  <!-- Tip Amount
-  / person
-
-  Total
-  / person
-
-  Reset -->
-  <div class="green">This is where the result gets shown</div>
+  <div class="green">
+    <div class="result-screen">
+      <div class="row">
+        <div>
+          <p class="title">Tip Amount</p>
+          <small>/ person</small>
+        </div>
+        <div class="result">$4.27</div>
+      </div>
+      <div class="row">
+        <div>
+          <p class="title">Total</p>
+          <small>/ person</small>
+        </div>
+        <div class="result">$32.79</div>
+      </div>
+    </div>
+    <ResetButton />
+  </div>
 </template>
 
 <style scoped>
 .green {
   display: flex;
-  align-items: center;
-  justify-content: center;
+  flex-direction: column;
+  justify-content: space-between;
   height: 300px;
+  padding: 2rem;
   background-color: hsl(183, 100%, 15%);
   color: hsl(172, 67%, 45%);
   border-radius: 0.8rem;
+}
+
+.row {
+  display: flex;
+  justify-content: space-between;
+  margin-bottom: 1rem;
+}
+
+.title {
+  color: hsl(0, 0%, 100%);
+  margin-bottom: 0;
+}
+
+small {
+  color: hsl(186, 14%, 43%);
+}
+
+.result {
+  font-size: 32px;
 }
 </style>
